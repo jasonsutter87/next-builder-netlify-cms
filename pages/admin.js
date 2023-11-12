87d4d278e7a09595f550752d0255f584bb100d35
Jason Sutter <jasonsutter87@gmail.com>
2023-11-11 17:51:43 -0800

    build complete

--- pages/admin.js
+++ pages/admin.js
@@ -1,2 +1,24 @@
-import AdminPage from './admin';
+// pages/admin.js
+import React from 'react';
+import fs from 'fs';
+import path from 'path';
+
+const AdminPage = ({ adminHtml }) => {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: adminHtml }} />
+  );
+};
+
+export async function getStaticProps() {
+  // Read the HTML content from public/admin/index.html
+  const filePath = path.join(process.cwd(), 'public', 'admin', 'index.html');
+  const adminHtml = fs.readFileSync(filePath, 'utf8');
+
+  return {
+    props: {
+      adminHtml,
+    },
+  };
+}
+
 export default AdminPage;
